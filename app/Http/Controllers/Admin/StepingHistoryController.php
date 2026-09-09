3dbcef6cf6b32ad4924f775f53cd13d72cc2102f
hasanarofid <hasanarofid@gmail.com>
2026-09-09 16:41:27 +0700

    fix: add missing BonusLog import in StepingHistoryController

--- app/Http/Controllers/Admin/StepingHistoryController.php
+++ app/Http/Controllers/Admin/StepingHistoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\BonusLog;
 use App\Models\TeamPointRedemption;
 use App\Models\User;
 use Illuminate\Http\Request;
