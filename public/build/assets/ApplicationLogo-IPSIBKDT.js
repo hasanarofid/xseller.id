359dac10a4fee40150315db90cadf0dcaa6fcee1
hasanarofid <hasanarofid@gmail.com>
2026-09-22 13:46:09 +0700

    build: rebuild assets - penyesuaian PO points & tier alokasi

--- public/build/assets/ApplicationLogo-IPSIBKDT.js
+++
@@ -1,1 +0,0 @@
-import{O as e,y as t}from"./app-Db48ZfU9.js";import{t as n}from"./_plugin-vue_export-helper-BDNMzG2s.js";var r={},i={src:`/logo-x-seller.avif`,alt:`XSELLER Logo`,class:`h-9 w-auto object-contain`};function a(n,r){return e(),t(`img`,i)}var o=n(r,[[`render`,a]]);export{o as t};
